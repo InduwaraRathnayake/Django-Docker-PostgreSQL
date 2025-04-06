fb6a60849e97b5217e143a623cd84de960bfd655
Induwara Rathnayake <rathnayakeinduwara23@gmail.com>
2025-04-06 18:18:43 +0530

    initial commit

---
+++ postgres/init.sql
@@ -0,0 +1,21 @@
+-- create a table for user information
+CREATE TABLE users (
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username TEXT NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- insert 10 users with different details
+INSERT INTO users (username, email, password) VALUES
+('user1', 'user1@example.com', 'password1'),
+('user2', 'user2@example.com', 'password2'),
+('user3', 'user3@example.com', 'password3'),
+('user4', 'user4@example.com', 'password4'),
+('user5', 'user5@example.com', 'password5'),
+('user6', 'user6@example.com', 'password6'),
+('user7', 'user7@example.com', 'password7'),
+('user8', 'user8@example.com', 'password8'),
+('user9', 'user9@example.com', 'password9'),
+('user10', 'user10@example.com', 'password10');
